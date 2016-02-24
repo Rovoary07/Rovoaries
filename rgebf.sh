0b28acbf3f71f54b2920dcce0a8fafdf6a7bfb90
Rovoary07 <rovoary07@gmail.com>
2016-02-24 13:35:33 +0700

    Update rgebf.sh

--- rgebf.sh
+++ rgebf.sh
@@ -1,14 +1,26 @@
 #!/bin/sh
 
-chmod a+x RGEbrute
+
+sudo chmod a+x RGEbrute
+
+#set color
+Color_Off='\033[0m'
+
+BWhite='\033[1;37m'
+BRed='\033[1;31m'
+BBlue='\033[1;34m'
+BGreen='\033[1;32m'
+
+clear
+echo "$BBlue"
 echo "      _______   _______   _______   _______   _______"
 echo "     /  __  /  /   ___/  /  ____/  /   _  /  /  ____/" 
 echo "    / /__/ /  /   /     /  /___   /  /_/ /  /  /__"
 echo "   / /  __/  /  _/__   /  ____/  /  ____/  /  ___/"
 echo "  / / \ \   /  /_  /  /  /      /   _ \   /  /"
 echo " / /   \ \ /      /  /  /____  /  /_/ /  /  /"
-echo "/ /     \ \ _____/  /_______/ /______/  /__/"
-echo "\n\tCreate by Rovoaries G Estraiw"
+echo "/ /     \ \ _____/  /_______/ /______/  /__/$Color_Off"
+echo "\n\t$BRed Create by$Color_Off : $BWhite Rovoaries G Estraiw$Color_Off"
 
 echo "\n\t[Ex. /root/desktop/001.cap ]"
 echo -n "\tPath handshake file: "
@@ -21,26 +33,25 @@ echo -n "\tEnd lenge : "
 read end
 
 clear
-
+echo "$BBlue"
 echo "      _______   _______   _______   _______   _______"
 echo "     /  __  /  /   ___/  /  ____/  /   _  /  /  ____/" 
 echo "    / /__/ /  /   /     /  /___   /  /_/ /  /  /__"
 echo "   / /  __/  /  _/__   /  ____/  /  ____/  /  ___/"
 echo "  / / \ \   /  /_  /  /  /      /   _ \   /  /"
 echo " / /   \ \ /      /  /  /____  /  /_/ /  /  /"
-echo "/ /     \ \ _____/  /_______/ /______/  /__/"
+echo "/ /     \ \ _____/  /_______/ /______/  /__/$Color_Off"
+echo "\n\t$BRed Create by$Color_Off : $BWhite Rovoaries G Estraiw$Color_Off"
 
-echo "\n\tCreate by Rovoaries G Estraiw"
-echo "\tPath handshake file path : $handshake"
-echo "\tEnter Bssid : $bssid"
-echo "\tStart lenge : $start"
-echo "\tEnd lenge : $end"
+echo "\n\t$BGreen[*] Infomation$Color_Off"
+echo "\tPath handshake file path :$BBlue $handshake $Color_Off"
+echo "\tEnter Bssid :$BBlue $bssid $Color_Off"
+echo "\tStart lenge :$BBlue $start $Color_Off"
+echo "\tEnd lenge :$BBlue $end $Color_Off"
 
-echo "\t[*] Check your information........."
+echo "\t$BGreen[*] Check your information........."
 sleep 5
-echo "\t[!] Start now!"
+echo "\t[!] Start now!$Color_Off"
 sleep 2
 
 ./RGEbrute $start $end | aircrack-ng $handshake --bssid $bssid -w -
-
-clear
